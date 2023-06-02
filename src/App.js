1273f729de65c97dba3b2a3d169bb1bf30566f21
nagyeong kim <skrud0318@naver.com>
2023-06-02 14:39:32 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -6,27 +6,28 @@ import UserPage from './Userpage';
 import Dobby from './Dobby';
 import Report from './Report';
 import Working from './Working';
+import './styles.css';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <div>
+        
+          
+            <div className="nav">
               <Link to="/userpage">마이페이지</Link>
             </div>
-            <div>
+            <div className="nav">
               <Link to="/dobby">커뮤니티</Link>
             </div>
-            <div>
+            <div className="nav">
               <Link to="/report">보고서</Link>
             </div>
-            <div>
+            <div className="nav">
               <Link to="/working">근로</Link>
             </div>
-          </ul>
-        </nav>
+       
+     
 
         <Routes>
           <Route path="/userpage" element={<UserPage />} />
