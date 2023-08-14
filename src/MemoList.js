fb8ca3e15681228cc5153cf148633e571e286a83
nagyeong kim <skrud0318@naver.com>
2023-08-14 10:56:00 +0900

    git react-icon 강제 설치 전 commit

--- src/MemoList.js
+++ src/MemoList.js
@@ -1,53 +1,79 @@
-// import React, {useMemo} from "react";
-// import { useNavigate } from "react-router-dom";
-// import {useTable} from "react-table";
-// import MEMO_DATA from "./MemoTable/MEMO_DATA.json";
-// import {COLUMNS} from "./MemoTable/columns"
-// import './MemoList.css';
-
-// function MemoList (){
-//     const columns = useMemo(()=>COLUMNS, []);
-//     const data = useMemo(()=>MEMO_DATA, []);
-//     const tableInstance = useTable({columns,data});
-//     const movePage = useNavigate();
-
-//     function goMain() {
-//         movePage('/Attendance');
-//     }
-
-
-
-//     return (
-//       <table>
-//         <thead>table
-//           <tr>
-//             <th></th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <tr></tr>
-//         </tbody>
-//       </table>
-//     );
-// }
-// export default MemoList;
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
 import DATA_JSON from "./MemoTable/MEMO_DATA.json"; // 데이터 파일 경로를 맞게 수정해주세요
-import { COLUMNS } from "./MemoTable/columns";
-import './MemoList.css';
-
+import { IconName } from "react-icons/ai";
+import "./MemoList.css";
 
 function MemoList() {
-  const columns = useMemo(() => COLUMNS, []);
+  //studentInfo에 변경이 있을 때만 업데이트
+  //accessor와 받아오는 data keyname이 같아야함
+  const columnData = [
+    {
+      Header: "Date", // "Date"는 헤더 이름입니다.
+      accessor: "data", // "data"는 해당 열에 표시할 데이터의 키입니다.
+    },
+    {
+      Header: "Memo",
+      accessor: "memo",
+    },
+  ];
+  const columns = useMemo(() => columnData, []);
   const data = useMemo(() => DATA_JSON, []); // 데이터를 DATA_JSON에서 가져옴
   const tableInstance = useTable({ columns, data });
   const movePage = useNavigate();
 
   function goMain() {
-    movePage('/Attendance');
+    movePage("/Attendance");
   }
+
+  const [value, setValue] = useState("");
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  const fullNameHeaderClass = "fullNameHeader";
+
+  // 체크박스가 체크되었는지 여부를 관리하는 상태
+  const [checkedRows, setCheckedRows] = useState([]);
+
+  // 체크박스를 클릭할 때마다 해당 행의 인덱스를 상태에 추가 또는 제거하는 함수
+  const handleCheckboxChange = (rowIndex) => {
+    if (checkedRows.includes(rowIndex)) {
+      setCheckedRows((prev) => prev.filter((index) => index !== rowIndex));
+    } else {
+      setCheckedRows((prev) => [...prev, rowIndex]);
+    }
+  };
+  //radio를 클릭하면 인덱스 받아오기
+  const handleRadioChange = (rowIndex) => {
+    setSelectedRow(rowIndex);
+  };
+
+  const [studentInfo, setstudentInfo] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 번호
+  const [pageSize, setPageSize] = useState(5); //한페이지에 보여줄 페이지개수
+
+  // 현재 페이지에 해당하는 데이터를 가져오는 함수
+  const getCurrentPageData = () => {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    return data.slice(startIndex, endIndex);
+  };
+
+  // 다음 페이지로 이동하는 함수
+  const goToNextPage = () => {
+    setCurrentPage((prev) => prev + 1);
+  };
+
+  // 이전 페이지로 이동하는 함수
+  const goToPrevPage = () => {
+    setCurrentPage((prev) => prev - 1);
+  };
+
+  // 현재 페이지에 해당하는 데이터를 가져옵니다.
+  const currentPageData = useMemo(
+    () => getCurrentPageData(),
+    [data, currentPage]
+  );
 
   const {
     getTableProps,
@@ -55,34 +81,56 @@ function MemoList() {
     headerGroups,
     rows,
     prepareRow,
-  } = tableInstance;
+    page,
+    state: { pageIndex, pageCount },
+  } = useTable(
+    {
+      columns,
+      data: currentPageData,
+      initialState: { pageIndex: 0, pageSize },
+    },
+    usePagination
+  );
 
   return (
     <div>
-      <button onClick={goMain}>Go to Attendance</button>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <div></div>
+      <div>
+        <div>
+          <table {...getTableProps()} className="tableWithBorders">
+            {" "}
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row, rowIndex) => {
+                prepareRow(row);
+                const isRowSelected = rowIndex === selectedRow;
+                return (
+                  <tr key={rowIndex} id="rowFont" {...row.getRowProps()}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <button
+            onClick={goToPrevPage}
+            id="tableLeftBtn"
+            disabled={currentPage === 1}
+          >
+            {" << "}prev
+          </button>
+          <button
+            id="tableRightBtn"
+            onClick={goToNextPage}
+            disabled={currentPage === Math.ceil(data.length / pageSize)}
+          >
+            next{" >> "}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
