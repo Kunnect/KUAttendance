7df896bf31de96333d022cf78b68c51cd24fe081
nagyeong kim <skrud0318@naver.com>
2023-08-30 13:53:23 +0900

    trash

--- src/report.js
+++ src/report.js
@@ -1,24 +1,11 @@
-//src/login.js
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./App.css";
+import React from 'react';
 
-function WorkingReport() {
-  const movePage = useNavigate();
-
-  function goMain() {
-    movePage("/Attendance");
-  }
-  function goMemo() {
-    movePage("/memo");
-  }
-
+const Working = () => {
   return (
-    <div className="report">
-      <p>보고서 페이지입니다</p>
-      <button onClick={goMain}>출근부</button>
-      <button onClick={goMemo}>메모장</button>
+    <div>
+      <h1>Report2</h1>
     </div>
   );
-}
-export default WorkingReport;
+};
+
+export default Working;
